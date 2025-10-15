a17fac550b21bfd7d91b221cc3e2b0f085d97416
orla-rey <orla.o.reynolds@gmail.com>
2025-10-14 20:32:22 -0400

    added axios

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary () {
     const [keyword, setKeyword] = useState("");
 
+    function handleResponse(response) {
+        console.log(response.data[0]);
+    }
+
+    
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
+
+        const apiKey = "1c21507bfe367ft66ea583e00ofa422f";
+        const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
+
     }
 
     function handleKeywordChange(event) {
