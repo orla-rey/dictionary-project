b0c5c54d7e783e0ab18b7173218a57890a20f529
orla-rey <orla.o.reynolds@gmail.com>
2025-10-14 20:20:22 -0400

    added basic search engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import Dictionary from './Dictionary';
+import './App.css';
 
 export default function App() {
 
   return (
-    <h1>Dictionary Search Engine</h1>
-  )
+    <div className='App'>
+    <div className='container'>
+    <header className='App-header'>Dictionary Search Engine</header>
+    <main><Dictionary /></main>
+    <footer className='App-footer'>This project was coded by <a href="https://github.com/orla-rey" target='blank'>Orla Reynolds</a>, is <a href="https://github.com/orla-rey/dictionary-project.git" target='blank'>open-sourced on GitHub</a> and <a href="https://dictionary-engine.netlify.app" target='blank'>hosted on Netlify</a></footer>
+    </div>
+    </div>
+  );
 }
