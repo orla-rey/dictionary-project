a69725c4787ef14e5042e9a8c6429d50297a3f0b
orla-rey <orla.o.reynolds@gmail.com>
2025-10-16 14:01:33 -0400

    added data for phonetics

--- src/Results.jsx
+++ src/Results.jsx
@@ -4,7 +4,12 @@ import Meaning from "./Meaning";
 export default function Results(props) {
     if (props.results) {
 return  <div className="Results"> 
+    <section>
     <h2>{props.results.word}</h2>
+    <div className="Phonetic">
+        Phonetic: {props.results.phonetic}
+    </div>
+    </section>
     {props.results.meanings.map(function(meaning, index) {
         return (
             <div key={index}>
